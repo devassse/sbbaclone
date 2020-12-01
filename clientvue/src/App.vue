7b0ab99a4f0390188fc71848d7b78c9416c204ce
devasssse <joao.mucavel@itgest.co.mz>
2020-12-01 15:56:36 +0200

    vue client

--- clientvue/src/App.vue
+++ clientvue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
+  <section id="app">
     <!-- Top and Aside Menu Container -->
-    <section>
+    <section id="navigators">
       <!-- Header Section -->
       <header class="app-main-header">
         <div class="wrapper">
@@ -15,37 +15,33 @@
       <!--/ imported aside -->
     </section>
     <!--/ Top and Aside Menu Container -->
+    <!-- App Main Container -->
+    <section id="main-container">
+    
+      <Dashboard />
 
-    <img
-      alt="Vue logo"
-      src="./assets/imgs/logos/sb-shield-02.png"
-      style="width: 20%"
-    />
-    <HelloWorld msg="Lorem ipsum dolor sit amet" />
-  </div>
+    </section>
+    <!--/ App Main Container -->
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Navbar from "./components/Navbar.vue";
 import Aside from "./components/Aside.vue";
+
+import Dashboard from './components/Dashboard';
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     Aside,
     Navbar,
+    Dashboard
     
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
+
 </style>
